83b771d9d36cce10f29d933fff686f9931303acd
Enrique <edavalos@ucsc.edu>
2020-06-30 21:37:32 -0700

    More methods to determine statement type

--- src/com/edavalos/stamp/Parser.java
+++ src/com/edavalos/stamp/Parser.java
@@ -18,12 +18,17 @@ public final class Parser {
 
             String firstWord = (statement.split(" ")[0]).toUpperCase();
 
-            if (statement.equals("+")) {
-
-            }
+//            if (statement.equals("+")) {
+//                enterBlock();
+//            }
 
             if (statement.equals("-")) {
+                leaveBlock();
+            }
 
+            if (firstWord.equals("ON") && statement.endsWith(":")) {
+                addBlock(statement, line);
+                continue;
             }
 
             if (statement.contains("=") && !statement.contains("==")) {
@@ -31,23 +36,26 @@ public final class Parser {
                 continue;
             }
 
-            for (LoopType loop : LoopType.values()) {
-                if (loop.name().equals(firstWord)) {
-                    if (!statement.endsWith(":")) {
-                        System.out.println("[ERR] " + Main.fileName + ":" + ((int) line) + " -> Loop '" + statement + "' should end with ':'");
-                        System.exit(0);
-                    }
-                    else {
-                        addLoop(statement, line);
-                        continue;
-                    }
-                }
+            boolean isLoop = (firstWord.equals("LOOP") || firstWord.equals("WHILE") || firstWord.equals("IF"));
+            if (!statement.endsWith(":") && isLoop) {
+                System.out.println("[ERR] " + Main.fileName + ":" + ((int) line) + " -> Loop '" + statement + "' should end with ':'");
+                System.exit(0);
+            }
+
+            if (statement.endsWith(":") && !isLoop) {
+                System.out.println("[ERR] " + Main.fileName + ":" + ((int) line) + " -> Loop '" + statement + "' is not a valid loop.");
+                System.exit(0);
+            }
+
+            if (statement.endsWith(":") && isLoop) {
+                addLoop(statement, line);
+                continue;
             }
 
             for (Action act : Action.values()) {
                 if (act.name().equals(firstWord)) {
                     addFunc(statement, line);
-                    continue;
+                    break;
                 }
             }
 
